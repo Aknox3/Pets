d00149c9694f61577d502424ecb4a13a13f25921
Mathias Taylor <mtaylor53@mail.greenriver.edu>
2018-01-11 17:47:48 -0500

    The next to the next t the next commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     //turn on error reporting
     ini_set('display_errors',1);
     error_reporting(E_ALL);
-    require_once "vendor/autoload.php";
+    require_once ('vendor/autoload.php');
 
     $f3 = Base::instance();
 
